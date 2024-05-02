18a58066c6ed9d424288193313daa5b61692caf3
Sven Wappler <wappler@wappler.systems>
2024-05-03 00:28:43 +0200

    Merge https://github.com/gigabit-gmbh/cleverreach/tree/feat/oauth-integration

--- Classes/Service/ConfigurationService.php
+++ Classes/Service/ConfigurationService.php
@@ -19,8 +19,7 @@ use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
 class ConfigurationService
 {
 
-
-    public function getConfiguration()
+    public function getConfiguration(): array
     {
 
         $configurationManager = GeneralUtility::makeInstance(ConfigurationManagerInterface::class);
@@ -28,13 +27,14 @@ class ConfigurationService
             ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT,
             'cleverreach'
         );
+
         return $settings['plugin.']['tx_cleverreach.']['settings.'];
     }
 
     /**
      * @return string
      */
-    public function getRestUrl()
+    public function getRestUrl(): string
     {
 
         $config = $this->getConfiguration();
@@ -44,7 +44,7 @@ class ConfigurationService
     /**
      * @return string
      */
-    public function getClientId()
+    public function getClientId(): string
     {
 
         $config = $this->getConfiguration();
@@ -55,7 +55,7 @@ class ConfigurationService
     /**
      * @return string
      */
-    public function getLoginName()
+    public function getLoginName(): string
     {
         $config = $this->getConfiguration();
         return $config['login'];
@@ -64,7 +64,7 @@ class ConfigurationService
     /**
      * @return string
      */
-    public function getPassword()
+    public function getPassword(): string
     {
         $config = $this->getConfiguration();
         return $config['password'];
@@ -73,7 +73,7 @@ class ConfigurationService
     /**
      * @return int
      */
-    public function getGroupId()
+    public function getGroupId(): string
     {
         $config = $this->getConfiguration();
         return (int)$config['groupId'];
@@ -82,7 +82,7 @@ class ConfigurationService
     /**
      * @return int
      */
-    public function getFormId()
+    public function getFormId(): string
     {
         $config = $this->getConfiguration();
         return (int)$config['formId'];
@@ -91,10 +91,50 @@ class ConfigurationService
     /**
      * @return string
      */
-    public function getUnsubscribeMethod()
+    public function getUnsubscribeMethod(): string
     {
         $config = $this->getConfiguration();
         return $config['unsubscribemethod'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthMode(): string
+    {
+        return $this->getConfiguration()['authMode'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getOauthTokenUrl(): string
+    {
+        return $this->getConfiguration()['oauthTokenUrl'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getOauthClientId(): string
+    {
+        return $this->getConfiguration()['oauthClientId'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getOauthClientSecret(): string
+    {
+        return $this->getConfiguration()['oauthClientSecret'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getOauthClientCode(): string
+    {
+        return $this->getConfiguration()['oauthClientCode'];
     }
 
 }
